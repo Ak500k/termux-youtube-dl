c48d54a4e48bc5be8284634a116c01263dca559b
B³ŋ¢ħøN4uŧ <github@b1nch0.us.to>
2023-02-24 13:24:55 +0100

    indent | piping | py/pip installer

--- termux-ydl.sh
+++ termux-ydl.sh
@@ -2,18 +2,18 @@
 
 # Colors
 # ----------------------------------------
- BL='\e[01;90m' > /dev/null 2>&1; # Black
- R='\e[01;91m' > /dev/null 2>&1; # Red
- G='\e[01;92m' > /dev/null 2>&1; # Green
- Y='\e[01;93m' > /dev/null 2>&1; # Yellow
- B='\e[01;94m' > /dev/null 2>&1; # Blue
- P='\e[01;95m' > /dev/null 2>&1; # Purple
- C='\e[01;96m' > /dev/null 2>&1; # Cyan
- W='\e[01;97m' > /dev/null 2>&1; # White
- LG='\e[01;37m' > /dev/null 2>&1; # Light Gray
- N='\e[0m' > /dev/null 2>&1; # Null
- L='\033[7m' > /dev/null 2>&1; #Lines
- X='\033[0m' > /dev/null 2>&1; #Closer
+ BL='\e[01;90m' &>/dev/null ; # Black
+ R='\e[01;91m'  &>/dev/null ; # Red
+ G='\e[01;92m'  &>/dev/null ; # Green
+ Y='\e[01;93m'  &>/dev/null ; # Yellow
+ B='\e[01;94m'  &>/dev/null ; # Blue
+ P='\e[01;95m'  &>/dev/null ; # Purple
+ C='\e[01;96m'  &>/dev/null ; # Cyan
+ W='\e[01;97m'  &>/dev/null ; # White
+ LG='\e[01;37m' &>/dev/null ; # Light Gray
+ N='\e[0m'      &>/dev/null ; # Null
+ L='\033[7m'    &>/dev/null ; #Lines
+ X='\033[0m'    &>/dev/null ; #Closer
 # ----------------------------------------
  clear
  echo -e "------------------------------------------------\n"
@@ -58,8 +58,9 @@ echo -e "\n\n--------------------------------------------------------"
 
  echo -e $Y$L"\n Installing python... "$N
 
-		apt install python -y
-                /data/data/com.termux/files/usr/bin/python3 -m pip install --upgrade pip
+		apt install python --no-install-recommends -y
+        pkg install --no-install-recommends -y python-pip
+        #        /data/data/com.termux/files/usr/bin/python3 -m pip install --upgrade pip
 		clear
  sleep 0.2		
 
